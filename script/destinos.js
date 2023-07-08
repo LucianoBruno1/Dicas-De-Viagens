f62253def26ec7a46f66fef8593a759dada42a35
Luciano Bruno <luciaanobruno@gmail.com>
2023-07-08 14:21:06 -0300

    Filtro de destinos com btn/adição de novos locais

--- script/destinos.js
+++ script/destinos.js
@@ -1,89 +1,124 @@
-// Dados do JSON
+
 var destinos = [
     {
       "id": "sa",
       "nome": "Macchu-Picchu",
       "descricao": "Machu Picchu, Peru",
-      "imagem": "../assets/images/images-destinys/machu-picchu.jpg"
+      "imagem": "../assets/images/images-destinys/machu-picchu.jpg",
+      "continente": "América do Sul"
     },
     {
         "nome": "Lençóis maranhenses",
         "descricao": "fodase",
-        "imagem": "../assets/images/images-destinys/lençois-maranhenses.jpg"
+        "imagem": "../assets/images/images-destinys/lençois-maranhenses.jpg",
+        "continente": "América do Sul"
     },
     {
       "nome": "Ilha de páscoa",
       "descricao": "Machu Picchu, Peru",
-      "imagem": "../assets/images/images-destinys/ilha-de-pascpa.jpg"
+      "imagem": "../assets/images/images-destinys/ilha-de-pascpa.jpg",
+      "continente": "América do Sul"
     },
     {
       "nome": "Cataratas do Iguaçu",
       "descricao": "Machu Picchu, Peru",
-      "imagem": "../assets/images/images-destinys/cataratas-do-iguaçu.jpg"
+      "imagem": "../assets/images/images-destinys/cataratas-do-iguaçu.jpg",
+      "continente": "América do Sul"
     },
     {
       "nome": "Ushuaia",
       "descricao": "Machu Picchu, Peru",
-      "imagem": "../assets/images/images-destinys/ushuaia.jpg"
+      "imagem": "../assets/images/images-destinys/ushuaia.jpg",
+      "continente": "América do Sul"
     },
     {
       "nome": "Gramado",
       "descricao": "Machu Picchu, Peru",
-      "imagem": "../assets/images/images-destinys/gramado.jpg"
+      "imagem": "../assets/images/images-destinys/gramado.jpg",
+      "continente": "América do Sul"
     },
     {
       "nome": "Colônia del sacramento",
       "descricao": "Machu Picchu, Peru",
-      "imagem": "../assets/images/images-destinys/colonia-del-sacramento.jpg"
+      "imagem": "../assets/images/images-destinys/colonia-del-sacramento.jpg",
+      "continente": "América do Sul"
     },
     {
       "nome": "Islas del rosario",
       "descricao": "Machu Picchu, Peru",
-      "imagem": "../assets/images/images-destinys/islas-del-rosario.jpg"
+      "imagem": "../assets/images/images-destinys/islas-del-rosario.jpg",
+      "continente": "América do Sul"
     },
     {
       "nome": "Cartagena",
       "descricao": "Machu Picchu, Peru",
-      "imagem": "../assets/images/images-destinys/cartagena.jpeg"
+      "imagem": "../assets/images/images-destinys/cartagena.jpeg",
+      "continente": "América do Sul"
     },
     {
       "nome": "Paris",
       "descricao":"teste",
-      "imagem": "../assets/images/images-destinys/Paris.jpg"
+      "imagem": "../assets/images/images-destinys/Paris.jpg",
+      "continente": "Europa"
+    },
+    {
+      "nome": "Uyuni",
+      "descricao":"teste",
+      "imagem": "../assets/images/images-destinys/uyuni.jpg",
+      "continente": "América do Sul"
+    },
+    {
+      "nome": "Torres del paine",
+      "descricao":"teste",
+      "imagem": "../assets/images/images-destinys/torres-del-paine.jpg",
+      "continente": "América do Sul"
+    },
+    {
+      "nome": "Peyto lake",
+      "descricao":"teste",
+      "imagem": "../assets/images/images-destinys/lago-peyto.jpg",
+      "continente": "América do Norte"
+    },
+    {
+    "nome": "Cataratas do niágara",
+    "descricao":"teste",
+    "imagem": "../assets/images/images-destinys/cataratas-do-niagara.jpg",
+    "continente": "América do Norte"
     }
   ];
   
   // Função para exibir os destinos na div "all-destinos"
   function exibirDestinos(filtro) {
     var allDestinosDiv = document.getElementById("all-destinos");
-    allDestinosDiv.innerHTML = ""; // Limpar destinos anteriores
+    allDestinosDiv.innerHTML = "";
   
-
-
-  
-    // Percorrer os destinos e criar elementos HTML
     destinos.forEach(function(destino) {
-      var destinoDiv = document.createElement("div");
-      destinoDiv.classList.add("destino");
-
-      var nome = document.createElement("h2");
-      nome.textContent = destino.nome;
-      destinoDiv.appendChild(nome);
+      if (!filtro || destino.continente === filtro) {
+        var destinoDiv = document.createElement("div");
+        destinoDiv.classList.add("destino");
   
-      var imagem = document.createElement("img");
-      imagem.src = destino.imagem;
-      destinoDiv.appendChild(imagem);
+        var nome = document.createElement("h2");
+        nome.textContent = destino.nome;
+        destinoDiv.appendChild(nome);
   
-      
+        var imagem = document.createElement("img");
+        imagem.src = destino.imagem;
+        destinoDiv.appendChild(imagem);
   
-      allDestinosDiv.appendChild(destinoDiv);
+        allDestinosDiv.appendChild(destinoDiv);
+      }
     });
   }
-  
-  // Chamar a função para exibir os destinos
   exibirDestinos();
-
-  document.getElementById("button-todos").addEventListener("click", function() {
-  exibirDestinos("todos");
-});
   
+  var filtroBtns = document.querySelectorAll(".filtro-btn");
+
+filtroBtns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    var filtro = btn.getAttribute("data-filtro");
+    exibirDestinos(filtro);
+  });
+});
+
+
+
